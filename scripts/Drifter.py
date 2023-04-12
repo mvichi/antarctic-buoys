48b038967c767f7222b119a30708bdbd25cd977a
Ashleigh Womack <42268606+AshleighWomack@users.noreply.github.com>
2023-04-12 13:21:26 +0200

    Update Drifter.py

--- scripts/Drifter.py
+++ scripts/Drifter.py
@@ -8,7 +8,10 @@ import numpy as np
 import pandas as pd
 from geopy.distance import distance
 
-#%%
+##################
+#%% Drift analysis
+##################
+
 def distance_from(lat0,lon0,lats,lons):
     '''
     Compute the geodetic distance from a reference point in meters
@@ -278,5 +281,216 @@ def abs_dispersion_discrete(drifter,freq='D',which='x'):
     A2 = drifter.groupby(pd.Grouper(freq=freq,
                                     label='right')).apply(abs_dispersion,which=which)
     return A2
+
+########################
+#%% Atmospheric response
+########################
+
+# Function to correct wind speed to 10 m reference values
+def W10(U,z,z0=0.0002,z0u=0.1):
+    """
+    This function adjusts the wind speed U measured at height z
+    to the corrected wind speed Uc which would be indicated
+    locally at 10 m above terrain with roughness z0.
+    All heights are in m and speeds in m/s.
+    z0u is the effective roughness length of the terrain
+        upstream of the measurement station
+    z0 is roughness length in the application (e.g. over the grid cell)
+
+    World Meteorological Organization 2014
+    Guide to Meteorological Instruments and Methods of Observation.
+    Geneva, Switzerland,
+    World Meteorological Organization, 1128p. (WMO-No.8, 2014)
+
+    """
+    CF = 1.  # flow distortion correction (1 if on a mast)
+    CT = 1.  # correction factor due to topographic effects (1 on the ocean)
+    corr = (np.log(60/z0u)*np.log(10/z0))/(np.log(10/z0u)*np.log(60/z0))
+    Uc = U*CF*CT*(np.log(10/z0u)/np.log(z/z0u))*corr
+    return Uc
+
+# Function to compute magnitude and direction from u,v vectors
+def uv2magdir(u,v):
+    """
+    Computes magnitude and direction from vector components
+    """
+    wm = np.sqrt(u**2+v**2)
+    wd = 270 - (180/np.pi)*np.arctan2(v,u) # trig angle to wind direction
+    wdcorr = np.where(wd<360,wd,wd-360)
+    return wm,wdcorr
+
+# Extract the e5 variables 
+def e5_variables(drifter, e5_file):
+    '''
+    Extract the ERA5 variables at the location of the drifter
+    
+    Parameters
+    ----------
+    drifter : pandas DataFrame
+        DESCRIPTION.
+    e5_file : NetCDF4 file
+        The file containing ERA5 mslp, 2m-Temp, 10m-winds (u, v)
+   Returns
+    -------
+    drifter : DataFrame
+        The updated DataFrame containing ERA5 reanalysis variables
+    '''
+    # create the variables
+    U10     = np.zeros(len(drifter))
+    U10_dir = np.zeros(len(drifter))
+    temp    = np.zeros(len(drifter))
+    mslp    = np.zeros(len(drifter))
+    u10     = np.zeros(len(drifter))
+    v10     = np.zeros(len(drifter))
+    n = 0
+    for i,j,t in zip(drifter['longitude (deg)'],drifter['latitude (deg)'],drifter.index):
+        u       = e5_file["u10"].sel(longitude=i,latitude=j,time=t,method='nearest').values
+        v       = e5_file["v10"].sel(longitude=i,latitude=j,time=t,method='nearest').values
+        U10[n], U10_dir[n] = uv2magdir(u,v) 
+        temp[n] = e5_file["t2m"].sel(longitude=i,latitude=j,time=t,method='nearest').values
+        mslp[n] = e5_file["msl"].sel(longitude=i,latitude=j,time=t,method='nearest').values
+        u10[n]  = e5_file["u10"].sel(longitude=i,latitude=j,time=t,method='nearest').values
+        v10[n]  = e5_file["v10"].sel(longitude=i,latitude=j,time=t,method='nearest').values
+        n=n+1
+    # add to variables to drifter DataFrame
+    drifter['U10_E5 (m/s)']      = U10         # modular speed of wind
+    drifter['U10_dir_E5 (degs)'] = U10_dir     # wind direction 
+    drifter['t2m_E5 (degC)']     = temp-273.15 # Kelvin to Celcius
+    drifter['mslp_E5 (hPa)']     = mslp/100.   # Pa to hPa
+    drifter['u10_E5 (m/s)']      = u10         # u-component of wind
+    drifter['v10_E5 (m/s)']      = v10         # v-component of wind
+    return drifter   
+
+#%% Create quiver plot for wind and drifter speeds and direction
+    
+def stick_plot(time, u, v, **kw):
+    width = kw.pop('width', 0.002)
+    headwidth = kw.pop('headwidth', 0)
+    headlength = kw.pop('headlength', 0)
+    headaxislength = kw.pop('headaxislength', 0)
+    angles = kw.pop('angles', 'uv')
+    ax = kw.pop('ax', None)
+    
+    if angles != 'uv':
+        raise AssertionError("Stickplot angles must be 'uv' so that"
+                             "if *U*==*V* the angle of the arrow on"
+                             "the plot is 45 degrees CCW from the *x*-axis.")
+
+    time, u, v = map(np.asanyarray, (time, u, v))
+    if not ax:
+        fig, ax = plt.subplots()
+    
+    q = ax.quiver(date2num(time), [[0]*len(time)], u, v,
+                  angles='uv', width=width, headwidth=headwidth,
+                  headlength=headlength, headaxislength=headaxislength,
+                  **kw)
+
+    ax.axes.get_yaxis().set_visible(False)
+    ax.xaxis_date()
+    return q
+
+
+#%% Compute the wind factor, turning angle and vector coefficient of determination
+import math    
+def drift_response(drifter):
+    '''
+    Relationship between the ice motions and ERA5 winds - 
+    adapted from Kimura and Wakatsuchi (2000), Kimura (2004), 
+    Fukamachi et al. (2011)
+    Computes the wind factor, turning angle and vector coefficient 
+    of determination between the wind speed and ice (drifter) speed.
+    We also compute the Pearon coefficient of determination.
+    Parameters
+    ----------
+    drifter : pandas DataFrame
+        Drifter DataFrame
+    Returns
+    -------
+    theta_degs, WF, R2v, R2p : numpy.float64
+    '''
+    # variables (component - mean)
+    u   = drifter['u (m/s)'] - np.mean(drifter['u (m/s)'])
+    v   = drifter['v (m/s)'] - np.mean(drifter['v (m/s)'])
+    u10 = drifter['u10_E5 (m/s)'] - np.mean(drifter['u10_E5 (m/s)'])
+    v10 = drifter['v10_E5 (m/s)'] - np.mean(drifter['v10_E5 (m/s)'])
+    
+    # sum of multiplication of variables
+    u10v  = np.nansum(u10*v)
+    v10u  = np.nansum(v10*u)
+    u10u  = np.nansum(u10*u)
+    v10v  = np.nansum(v10*v)
+    u10_2 = np.nansum(np.square(u10)) # u10^2
+    v10_2 = np.nansum(np.square(v10))
+    u_2   = np.nansum(np.square(u)) 
+    v_2   = np.nansum(np.square(v))
+    
+    # turning angle (angle from the wind to ice drift)
+    # it is positive on a cartesian coordinate system
+    # but needs to be negative on geographical coordinate system
+    #theta = -math.atan((u10v - v10u)/(u10u + v10v)) 
+    theta = -math.atan2(u10v - v10u, u10u + v10v) 
+    theta_degs = math.degrees(theta) # in degrees (neg due to left delfection)
+    
+    # wind factor     
+    c1 = (np.cos(theta))*(u10u)
+    c2 = (np.sin(theta))*(v10u)
+    c3 = (np.sin(theta))*(u10v)
+    c4 = (np.cos(theta))*(v10v)
+
+    WF = ((c1 + c2 - c3 + c4)/(u10_2 + v10_2))  
+    
+    # vector coefficient of determination
+    R2v = np.square((c1 + c2 - c3 + c4)/(np.sqrt(u10_2 + v10_2)*np.sqrt(u_2 + v_2)))
+
+    # Pearson coefficient of determination
+    R2p = np.square(scipy.stats.pearsonr(drifter['U10_E5 (m/s)'][1:], 
+                                                 drifter['U (m/s)'][1:]))
+    
+    return theta, theta_degs, WF, R2v, R2p
+
+# Use the drift repsonse measurements to estimate the underlying currents
+def current_estimation(drifter,theta, WF):
+    '''
+    Estimate the underlying current velcotiy components -
+    This method is taken from Kimura and Wakatsuchi (2000).
+    It relates sea-ice velocity (u,v), ERA5 wind velocity 
+    (u10,v10) for the same time, the wind factor, turning
+    angle and mean ocean current (Cu,Cv).
+    Parameters
+    ----------
+    drifter : pandas DataFrame
+        Drifter DataFrame
+    theta_degs : numpy.float64
+        Mean turning angle in degrees
+    WF : numpy.float64
+        Mean wind factor
+    Returns
+    -------
+    Cu, Cv : numpy.float64
+        Mean ocean current components
+    C : numpy.float64
+        Mean modular current speed
+    '''
+    # mean of the velocity components
+    u10 = np.mean(drifter['u10_E5 (m/s)']) # mean u of wind
+    v10 = np.mean(drifter['v10_E5 (m/s)']) # mean v of wind
+    u   = np.mean(drifter['u (m/s)'])      # mean u of ice
+    v   = np.mean(drifter['v (m/s)'])      # mean v of ice
+    
+    # Estimate the mean ocean current which is derived by subtracting 
+    # the wind effect from the sea-ice motion
+    # set up the three matrices
+    M1 = np.array([[u],[v]])
+    # apply the counter-clockwise rotation matrix
+    M2 = np.array([[np.cos(theta), np.sin(theta)],
+           [-np.sin(theta), np.cos(theta)]])
+    M3 = np.array([[u10],[v10]])
+    
+    # this is the dot product multiplication 
+    # multiply by 100 to get into cm/s
+    Cuv = (M1 - WF*(M2.dot(M3)))*100
+    C     = np.sqrt(Cuv[0]**2 + Cuv[1]**2)
+    
+    return Cuv, C
 
 # end of code
