218ce46fd3b8e814c6127c5973c869709bd2e8d6
Ashleigh Womack <42268606+AshleighWomack@users.noreply.github.com>
2023-05-10 14:59:54 +0200

    Update of diagnostics using met data from ERA5

--- scripts/Drifter_diagnostics_metE5.py
+++ scripts/Drifter_diagnostics_metE5.py
@@ -3,7 +3,7 @@
 """
 Created on Thu May  4 13:13:49 2023
 
-eads preprocessed files and ERA5 data for computing the 
+Reads preprocessed files and ERA5 data for computing the 
 drift response to atmospheric forcing:
 - Wind factor, turning angle 
 - Correlation (Pearson and vector)
@@ -180,7 +180,7 @@ def drift_response(drifter):
     # but needs to be negative on geographical coordinate system
     #theta = -math.atan((u10v - v10u)/(u10u + v10v)) 
     theta = -math.atan2(u10v - v10u, u10u + v10v) 
-    theta_degs = math.degrees(theta) # in degrees (neg due to left delfection)
+    theta_degs = math.degrees(theta) # in degrees (neg due to left deflection)
     
     # wind factor     
     c1 = (np.cos(theta))*(u10u)
@@ -432,7 +432,6 @@ ax2.set_xscale(value='log')
 ax2.set_xlabel('Period (hours)')
 ax2.xaxis.set_major_formatter(ticker.FuncFormatter(lambda y, _: '{:g}'.format(y)))
 
-
 peaks,freqs_peak = find_peaks(psd_x, height=None,threshold=0.0005)
 np.diff(peaks)
 period = 1/freqs_x[peaks]                                    # psd freq and peaks
@@ -440,111 +439,5 @@ xc = 1/period[12]                                            # change no. for li
 ax.axvline(x=xc, color='black', linewidth=3)
 ax2.grid(True, which='both')
 ax.yaxis.grid(which='both')
-args=dict(xy=(0.004,0.95),xycoords='axes fraction',fontsize=34)
-
-#%%
-'''
-Application of a Butterworth High-Pass Filter 
-This is a 12th order filter used to remove all frequencies below the cutoff
-frequency of 0.04 Hz which is approximately the daily frequency, allowing 
-the sub-daily frequencies to become more significant. 
-'''
-b, a = signal.butter(12, 0.04, 'high', Fs)                  # 12th order with 0.04 h-1 cutoff frequency            
-w,h = signal.freqs(b,a)                                     # frequency response of a digital filter
-x_filt = signal.filtfilt(b,a,drifter['u (m/s)'][1:], padtype=None)   # filter data series
-y_filt = signal.filtfilt(b,a,drifter['v (m/s)'][1:], padtype=None)   # filter data series
-
-#%% Repeat the Fast Fourier Transform to the filtered data series
-# FFT for x_filt (zonal component)
-NFFT = np.size(x_filt)                                      # FFT length (radix 2 number!)
-m = NFFT/2                                                  # number of discrete frequency bins
-fc = 1/(2*dt)                                               # the critical frequency
-f = fc*np.arange(0,int(m)+1)/m                              # the frequency bins (NFFT/2 +1 bins)
-X_filt = np.fft.fft(x_filt,NFFT)                            # FFT of the length NFFT
-
-Pxx_filt = np.zeros(np.size(x_filt))
-for j in range(len(X_filt)):
-    Pxx_filt[j] = abs(X_filt[j]*np.conj(X_filt[j]))/NFFT    # periodogram = |X|^2   
-Pxx_filt = Pxx_filt[0:int(m)+1]                             # from 0:m+1 
-Pxx_filt = np.append(Pxx_filt[0],Pxx_filt[1:int(m)+1]*2)    # from 1:m+1
-print((x_filt**2).sum())                                    # Parseval's theorem -total power computed in time domain
-print(Pxx_filt.sum())                                       # must be equal to total power computed in frequency domain
-freqs_filt = np.fft.fftfreq(len(x_filt))
-
-# FFT for y_filt (meridional component)
-NFFT = np.size(y_filt)                                      # FFT length (radix 2 number!)
-m = NFFT/2                                                  # number of discrete frequency bins
-fc = 1/(2*dt)                                               # the critical frequency
-f = fc*np.arange(0,int(m)+1)/m                              # the frequency bins (NFFT/2 +1 bins)
-Y_filt = np.fft.fft(y_filt,NFFT)                            # FFT of the length NFFT
-
-Pyy_filt = np.zeros(np.size(y_filt))
-for j in range(len(Y_filt)):
-    Pyy_filt[j] = abs(Y_filt[j]*np.conj(Y_filt[j]))/NFFT    # periodogram = |X|^2   
-Pyy_filt = Pyy_filt[0:int(m)+1]                             # from 0:m+1 
-Pyy_filt = np.append(Pyy_filt[0],Pyy_filt[1:int(m)+1]*2)    # from 1:m+1 
-print((y_filt**2).sum())                                    # Parseval's theorem -total power computed in time domain
-print(Pyy_filt.sum())                                       # must be equal to total power computed in frequency domain
-freqs_filt = np.fft.fftfreq(len(y_filt))
-
-#%% Plot of Frequency vs Power of the filtered spectrum
-mpl.rcParams['font.size'] = 25
-fig, ax = plt.subplots(figsize=(20,10))
-   
-ax.loglog(f[1:int(m)],Pxx_filt[1:int(m)], linewidth=2.5, color='royalblue', 
-            label='zonal filtered')   
-ax.loglog(f[1:int(m)],Pyy_filt[1:int(m)], linewidth=2.5, color='orange', 
-            label='meridional filtered') 
-ax.set_xlabel('Frequency (h$^{-1}$)')
-ax.set_ylabel('Power (m$^{2}$ s$^{-2}$)') 
-
-secax = ax.secondary_xaxis('top',functions=(invert, invert))
-secax.set_xscale('linear')
-secax.set_xlabel('Period (hours)')
-ax.grid(True, which='both')
-ax.legend()
-
-#%% Determine the PSD of filtered data series
-# Zonal component
-freqs_filt_x, psd_filt_x = signal.welch(x_filt,fs=Fs, window='hann',scaling='density', 
-                          nperseg=256, noverlap=0) 
-# Meridional component
-freqs_filt_y, psd_filt_y = signal.welch(y_filt,fs=Fs, window='hann',scaling='density', 
-                          nperseg=256, noverlap=0) 
-
-#%% Plot Frequency vs PSD of filtered spectrum
-mpl.rcParams['font.size'] = 25
-fig, ax = plt.subplots(figsize=(20,10))
-ax.loglog(freqs_filt_x, psd_filt_x, linewidth=2.5, color='royalblue', 
-          label='zonal filtered')
-ax.loglog(freqs_filt_y, psd_filt_y, linewidth=2.5, color='orange',
-          label='meridional filtered')
-ax.set_xlabel('Frequency (h⁻¹)')
-ax.set_ylabel('PSD (m² s⁻² h⁻¹)')
-
-secax = ax.secondary_xaxis('top',functions=(invert, invert))
-secax.set_xscale(value='linear')
-secax.set_xlabel('Period (hours)')
-secax.grid()
-ax.legend(loc=3)
-
-peaks,freqs_peak = find_peaks(psd_x, height=None,threshold=0.0005)
-np.diff(peaks)
-period = 1/freqs_x[peaks]                                       # psd freq and peaks
-xc = 1/period[1]                                                # change no. for line at inertial period
-#plt.axvline(x=xc, color='red', linewidth=2.5)
-ax.grid(True, which='both')
-
-#%% Linear PSD vs with High-Pass Filter
-mpl.rcParams['font.size'] = 25
-fig, ax = plt.subplots(figsize=(20,10))
-ax.plot(freqs_x,psd_x, linewidth=3, label='normal')             # this just for the U component
-ax.plot(freqs_filt_x,psd_filt_x, linewidth=3, label='filtered')
-plt.plot(w,abs(h), color='magenta', label='frequency filter response')
-plt.xlim(0,0.25)
-ax.set_xlabel('Frequency (h⁻¹)')
-ax.axis([0, 0.13, 0, 9])
-ax.set_ylabel('PSD (m² s⁻² h⁻¹)')
-plt.legend()
 
 # end of code
